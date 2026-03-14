26069be21b966fda8b3a9cbbc8211ed751a25c2a
burykinburyk2004-lang <burykin.buryk2004@gmail.com>
2026-03-14 16:25:37 +0300

    add class person

---
+++ DBFirst_Car_Site/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace DBFirst_Car_Site.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string City { get; set; }
+
+        public string Number { get; set; }
+
+        Person() {
+
+        }
+
+        public void Print()
+        {
+            Console.Write("Персона");
+        }
+
+    }
+}
